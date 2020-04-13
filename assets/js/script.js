5b4e4477d8d55914dc41c6ed4ed69746fa70e04b
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-04-12 21:54:47 -0500

    added questions array, started the html file and css file

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var timerEl = document.getElementById
+
+
+
+var questions = [
+    {
+        prompt: "Commonly used data types DO Not include:",
+        answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+    },
+    {
+        prompt: "The condition in an if/else statement is enclosed with ___.",
+        answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "3. parenthesis"
+    },
+    {
+        prompt: "Arrays in JavaScript can be used to store ___.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        prompt: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. commas", "2.curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes"
+    },
+    {
+        prompt: "A very useful toll used during development and debugging for printing content to the debugger is:",
+        answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log"
+    }
+]
+
+var score = 0;
+
+for(var i=0; i < questions.length; i++) {
+    var response = 
+}
